587e204cb59b3b94f458e5b3cdf638809ce57bbe
github username <github email address>
2019-02-05 15:45:23 -0500

    add skills section update fonts

--- javascript/app.js
+++ javascript/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   // https://stackoverflow.com/questions/45728818/how-to-smooth-scroll-to-an-id-after-clicking-a-link-from-another-page
-  $('#linkproj').click(function(event) {
+  $('.navlinks').click(function(event) {
      event.preventDefault();
      $('html, body').animate({
           scrollTop : $(this.hash).offset().top
